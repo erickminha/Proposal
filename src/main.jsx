1d0d36c96a93a90a18fb938a8b6410dc86de341e
Selecionai <erickminha@gmail.com>
2026-04-15 15:33:05 -0300

    Fix router imports in main entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  Link,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
 import AcceptInvite from './AcceptInvite.jsx'
 import PublicApplication from './PublicApplication.jsx'
+import PublicHome from './PublicHome.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
